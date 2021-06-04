235c7959f9baf943514b73539c4f0433201d96ea
Hui-Sin <20018354@myrp.edu.sg>
2021-06-04 10:07:40 +0800

    Exercise 2

--- app/src/main/java/sg/edu/rp/c346/id20018354/arraylist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/arraylist/MainActivity.java
@@ -3,12 +3,32 @@ package sg.edu.rp.c346.id20018354.arraylist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    ArrayList <String> fruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fruits= new ArrayList<String>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("cherry");
+
+
+        tv=findViewById(R.id.textViewItems);
+        String text = "Fruits\n";
+        text+="=====\n";
+        for(int i=0;i<fruits.size();i++){
+            text+= fruits.get(i) +"\n";
+        }
+        tv.setText(text);
     }
 }
